a163f64892758dd3d9737aae3a1362d3c7f90b5e
koralyuster <70564042+koralyuster@users.noreply.github.com>
2021-07-06 17:39:40 +0300

    fix css issue

--- wp-content/plugins/templately/core/admin/views/settings.php
+++ wp-content/plugins/templately/core/admin/views/settings.php
@@ -1,7 +1,7 @@
 <div class="templately-admin-body">
     <div id="templatelyAdmin">
         <div class="templately-admin-preloader" style="padding: 100px;min-height: 100%;box-sizing: border-box;text-align: center;">
-            <img width="130px" src="/https://koralyuster.github.io/GymForAll/wp-content/plugins/templately/core/admin/views/TEMPLATELY_ASSETSimages/loading-logo.gif" alt="">
+            <img width="130px" src="https://koralyuster.github.io/GymForAll/wp-content/plugins/templately/core/admin/views/TEMPLATELY_ASSETSimages/loading-logo.gif" alt="">
         </div>
     </div>
 </div>
